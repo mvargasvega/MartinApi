89369a49555e94d136ca59b83d226b781c06fd9d
mvargasvega <martin@digitalnest.org>
2018-03-12 17:47:30 -0700

    Changed port to 3000

--- server.js
+++ server.js
@@ -96,6 +96,6 @@ console.log("Hello MArtttt")
  **********/
 
 // listen on the port that Heroku prescribes (process.env.PORT) OR port 3000
-app.listen(process.env.PORT || 3015, function () {
-  console.log('Express server is up and running on http://localhost:3015/');
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Express server is up and running on http://localhost:3000/');
 });
